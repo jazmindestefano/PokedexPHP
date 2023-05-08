0cf2fc53c6b66a3595e3f1df4f98a6396eba6ae2
jazmin <jazminisabeldestefano@gmail.com>
2023-05-07 22:20:31 -0300

    añadir id cuando clickeo para el detalle del pokemon

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -41,7 +41,8 @@ $conn->close();
     <ul>
         <?php
             foreach ($resultado as $element) {
-                echo "<li class='pokemon-list'><a>" . $element['nombre'] .  "</a></li>" . "<br/>";
+                echo '<li class="pokemon-list"><a href="../pokemon-detalle/pokemon-detalle.php?id=' . $element['idPokemon'] . '">' . $element['nombre'] . '</a></li><br/>';
+
             }
         ?>
     </ul>
